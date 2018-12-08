9ab84bf581149666282e2a7b8f0be12de88ee33e
Diogo (Diogo) Ferreira Ramos <diogo.ramos@kcl.ac.uk>
2018-12-08 20:22:15 +0000

    Create user class

--- src/User.java
+++ src/User.java
@@ -1,3 +1,35 @@
 public class User {
+    private String userName;
+    private String homeLocation;
+    private ArrayList<Event> listOfEvents;
+    private ArrayList<User> listOfFriends;
+
+
+    User () {
+
+
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public String setUserName(String newUserName) {
+        userName = newUserName;
+        return userName;
+    }
+    public String getHomeLocation() {
+        return homeLocation;
+    }
+    public String setHomeLocation(String homeAddress) {
+        homeLocation = homeAddress;
+        return homeLocation;
+    }
+    public ArrayList<Event> getListOfEvents() {
+        return listOfEvents;
+    }
+    public ArrayList<Event> addEvent(Event event) {
+        listOfEvents.add(event);
+        return listOfEvents;
+    }
+
 
 }
